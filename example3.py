fce71bc795294beed869219d49c2e343cb3c1ee1
Edward Görlich <goerlich_e@tfs.pl>
2021-11-02 22:45:34 +0100

    mnist example

--- example3.py
+++ example3.py
@@ -1,12 +1,45 @@
 #mnist data
 #https://matplotlib.org/stable/users/installing.html  - python -m pip install -U matplotlib
-from keras.datasets import mnist
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from matplotlib import pyplot
+from tensorflow.keras.utils import to_categorical
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-digit = train_images[0]
 
-import matplotlib.pyplot as plt
-plt.imshow(digit, cmap=plt.cm.binary)
-plt.show()
-print(train_labels[0])
+# summarize loaded dataset
+print('Train: X=%s, y=%s' % (train_images.shape, train_labels.shape))
+print('Test: X=%s, y=%s' % (test_images.shape, test_labels.shape))
+# plot first few images
+for i in range(9):
+	# define subplot
+	pyplot.subplot(330 + 1 + i)
+	# plot raw pixel data
+	pyplot.imshow(train_images[i], cmap=pyplot.get_cmap('gray'))
+# show the figure
+pyplot.show()
+network = models.Sequential()
+network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
+network.add(layers.Dense(10, activation='softmax'))
+
+network.compile(optimizer='rmsprop',
+loss='categorical_crossentropy',
+metrics=['accuracy'])
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+
+print('test_loss:', test_loss)
+print('test_acc:', test_acc)
